61f28cd018113b458e177dc400f0183ecb5aea49
OnigiriJPN <yuzuhajintian@gmail.com>
2026-08-03 19:35:21 +0900

    Add RobloxUser and PlateTemplate interfaces

--- types/roblox.ts
+++ types/roblox.ts
@@ -1,1 +1,12 @@
+export interface RobloxUser {
+  id: number;
+  name: string;
+  displayName: string;
+}
 
+export interface PlateTemplate {
+  id: string;
+  name: string;
+  description: string;
+  icon: string;
+}
